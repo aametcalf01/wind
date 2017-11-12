e320aa8f2036bd25752594561de87b6a818fc68b
dhinckleuvm <33502806+dhinckleuvm@users.noreply.github.com>
2017-11-12 13:35:04 -0500

    Add files via upload

--- CS295/CS295_code/Better-Bestest_Code/dayAveraging.py
+++ CS295/CS295_code/Better-Bestest_Code/dayAveraging.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import numpy as np
+##import pandas as pd
 
 def timeAvg(ls) :
 ##    lsArr = np.array(ls)
@@ -29,32 +30,39 @@ def timeAvg(ls) :
 src = sys.argv[1]
 dest = sys.argv[2]
 
-#cols = ['Lat','Lon','Ele']+['YR--MODAHRMN','DIR','SPD','GUS','TEMP','ALT']
+cols = ['Lat','Lon','Ele']+['YR--MODAHRMN','DIR','SPD','GUS','TEMP','ALT']
 
 with open(src) as f:
     contentI = f.readlines()
+
+##contentI = pd.read_csv(src)
+##content = contentI.as_matrix().astype('str').tolist()
 
 data = []
 
 
 content = [contentI[x].split(',') for x in range(len(contentI))]
 
-loc = content[1][1:3]
-d = content[1][4]
+ofs = 0
+lofs = 1
+
+loc = content[lofs][(1-ofs):(3-ofs)]
+##print(content[0][(4-ofs)])
+d = content[lofs][(4-ofs)]
 temp = []
-temp.append(content[1][4:10])
-i=2
+temp.append(content[lofs][(4-ofs):(10-ofs)])
+i=lofs+1
 while (i < len(content)) :
-    if content[i][1:3]==loc and content[i][4][0:8]==d[0:8]:
-        temp.append(content[i][4:10])
+    if content[i][(1-ofs):(3-ofs)]==loc and content[i][(4-ofs)][0:8]==d[0:8]:
+        temp.append(content[i][(4-ofs):(10-ofs)])
         i+=1
     elif len(temp)>0 :
         eph = loc+[d[0:8]]
         
         for j in range(len(temp)) :
-            print(temp[j][0]+'|||')
-            print(temp[j][0][8:10]+'---')
-            print(temp[j][0][10:12]+'---')
+##            print(temp[j][0]+'|||')
+##            print(temp[j][0][8:10]+'---')
+##            print(temp[j][0][10:12]+'---')
             temp[j][0] = float(temp[j][0][8:10])*60+float(temp[j][0][10:12])
         evan = [[temp[x][0],temp[x][1],temp[x][2]] for x in range(len(temp))]
         dels = []
@@ -71,9 +79,20 @@ while (i < len(content)) :
         vecs = []
         for j in range(len(evan)) :
             vecs.append(float(evan[j][2])*np.array([np.sin(angs[j]),np.cos(angs[j])]))
-        avgWD = timeAvg([[evan[x][0],vecs[x]] for x in range(len(evan))])
-        # unpack vector
-        eph.append(avgWD)
+        nEvan = [[evan[x][0],vecs[x]] for x in range(len(evan))]
+        if len(nEvan) > 0 :
+            avgWD = timeAvg([[evan[x][0],vecs[x]] for x in range(len(evan))])
+##            print(str(avgWD[0])+','+str(avgWD[1]))
+    ##        eph.append(avgWD) # no unpack
+            # unpack vector
+            nAng = np.arctan(avgWD[0]/avgWD[1])
+            if avgWD[1] < 0 :
+                nAng += np.pi
+            eph.append(nAng)
+            eph.append(np.linalg.norm(avgWD))
+        else :
+            eph.append('***')
+            eph.append('***')
 
         for j in range(3,6) :
             evan = [[temp[x][0],temp[x][j]] for x in range(len(temp))]
@@ -93,14 +112,22 @@ while (i < len(content)) :
         data.append(eph)
         
         
-        loc = content[i][1:3]
-        d = content[i][4]
+        loc = content[i][(1-ofs):(3-ofs)]
+        d = content[i][(4-ofs)]
         temp = []
-        temp.append(content[i][5:9])
+        temp.append(content[i][(4-ofs):(10-ofs)])
         i+=1
     else :
         print('DaFuq?')
         i+=1
 
-# WRITE DATA
+f = open(dest,'w')
+
+for i in range(len(data)) :
+    f.write(data[i][0])
+    for j in range(1,len(data[i])) :
+        f.write(','+str(data[i][j]))
+    f.write('\n')
+
+f.close()
     
